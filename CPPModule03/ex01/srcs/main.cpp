7a33d16f81e17435ca1f356cdc277b1ff5e1a6f4
DoughnutsNCookies <chuahtseyung2002@gmail.com>
2022-08-21 14:02:37 +0800

    Fix ex00 and added ex01

--- CPPModule03/ex01/srcs/main.cpp
+++ CPPModule03/ex01/srcs/main.cpp
@@ -6,29 +6,29 @@
 /*   By: schuah <schuah@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/21 12:41:41 by schuah            #+#    #+#             */
-/*   Updated: 2022/08/21 13:58:18 by schuah           ###   ########.fr       */
+/*   Updated: 2022/08/21 13:57:45 by schuah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
 
 int main()
 {
-    ClapTrap a;
+    ScavTrap a("Bob");
 	std::cout << "name: " << a.getName() << std::endl;
     std::cout << "Hp: " << a.getHp() << std::endl;
     std::cout << "Ep: " << a.getEp() << std::endl;
     std::cout << "Ad: " << a.getAd() << std::endl;
     a.takeDamage(3);
-    ClapTrap b(a);
+    ScavTrap b(a);
     b.takeDamage(3);
-    ClapTrap c;
+    ScavTrap c;
     c = b;
     b.takeDamage(3);
 	b.takeDamage(999);
 	b.beRepaired(1);
 	b.beRepaired(100);
-	for (int i = 0; i < 10; i++)
+	for (int i = 0; i < 50; i++)
 		b.attack("noob");
 	return (0);
 }
